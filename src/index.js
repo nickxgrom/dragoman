df8fda1efc587a48618eff0edd3a93d78a47c9f1
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2020-12-20 01:26:06 +0600

    Add base script body

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import translate from 'translate';
 import { token }  from "../config.json"
 translate.key = token;
 
-translate('Hello world', 'ru')
-    .then( text => {
-        console.log(text)
-    } )
-    .catch( err => {
-        console.log(err)
-    } )
+const sourceInput = document.getElementById("source-input"),
+    targetInput = document.getElementById("target-input"),
+    translateBtn = document.querySelector(".translate-btn");
+
+translateBtn.addEventListener("click", () => {
+    translate(sourceInput.value, 'ru')
+        .then( text => {
+            targetInput.value = text;
+        } )
+        .catch( err => {
+            console.log(err)
+        } )
+})
