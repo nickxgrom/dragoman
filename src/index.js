bed7aa1fc7ba2dd5c79c85093d4f3cc545fe606d
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2020-12-21 00:54:48 +0600

    Release 1.0 translate feature

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import translate from 'translate';
 import { token }  from "../config.json"
+import generateOptions from "./scripts/selectOptionGenerator"
 translate.key = token;
+
+generateOptions();
 
 const sourceInput = document.getElementById("source-input"),
     targetInput = document.getElementById("target-input"),
-    translateBtn = document.querySelector(".translate-btn");
+    translateBtn = document.querySelector(".translate-btn"),
+    targetLanguage = document.getElementById("target-language");
 
 translateBtn.addEventListener("click", () => {
-    translate(sourceInput.value, 'ru')
+    translate(sourceInput.value, targetLanguage.value)
         .then( text => {
             targetInput.value = text;
         } )
