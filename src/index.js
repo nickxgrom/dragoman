e6945ec6a214d4e5af54313b4ee48ab075978585
nickxgrom <52834219+nickxgrom@users.noreply.github.com>
2020-12-22 18:43:43 +0600

    Add functionality to switch-language button

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ generateOptions();
 const sourceInput = document.getElementById("source-input"),
     targetInput = document.getElementById("target-input"),
     translateBtn = document.querySelector(".translate-btn"),
+    switchLangBtn = document.querySelector(".switch-languages"),
     sourceLanguage = document.getElementById("source-language"),
     targetLanguage = document.getElementById("target-language");
 
@@ -18,6 +19,24 @@ sourceInput.addEventListener("keydown", (event) => {
         translateBtn.focus();
     }
 })
+
+switchLangBtn.onclick = () => {
+    let selectedSourceOption = Array.from(sourceLanguage.childNodes).find( item => {
+        return item.selected;
+    }),
+        selectedTargetOption = Array.from(targetLanguage.childNodes).find( item => {
+        return item.selected;
+    })
+
+    let srcVal = selectedSourceOption.value,
+        srcTxt = selectedSourceOption.innerText;
+
+    selectedSourceOption.value = selectedTargetOption.value;
+    selectedSourceOption.innerText = selectedTargetOption.innerText;
+
+    selectedTargetOption.value = srcVal;
+    selectedTargetOption.innerText = srcTxt;
+}
 
 function translationEvent() {
     translate( sourceInput.value, { from: sourceLanguage.value, to: targetLanguage.value, } )
